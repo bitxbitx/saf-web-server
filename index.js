0a9d6485b613000ecbd6b1a3ce04d9c6ec59867b
Stanley Ng <sngg0055@student.monash.edu>
2023-05-21 14:41:44 +0800

    Accidentally fked up index.js

--- index.js
+++ index.js
@@ -1,190 +1,85 @@
-const ChatMessage = require('./models/livechat/chatMessage.model');
-const ChatSession = require('./models/livechat/chatSession.model');
-const socketIO = require('socket.io');
-const User = require('./models/user.model');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/error.middleware');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const socketServer = require('./socketServer'); // Import the Socket.io server
 
-module.exports = (server) => {
-  // saves all the connected clients
-  const clients = {};
+const port = process.env.PORT || 8000;
 
-  // Create a new instance of the Socket.io server
-  const io = socketIO(server,{
-    path: '/api/live-chat',
-  });
+connectDB();
 
-  // TODO : ADD ERROR HANDLING
+const app = express();
 
-  io.on('connection', async (socket) => {
-    console.log('New client connected');
+// Morgan
+app.use(morgan('dev'));
 
-    socket.on('init', async (userId) => {
-      // Fetch all active ChatSession for this user id and return them through emitting 'init' event
-      const chatSessions = await ChatSession.find({
-        participants: userId,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
+// Cookie Parser
+app.use(cookieParser());
 
-      // Add them to the clients object
-      clients[userId] = socket;
-      console.log('clients.length', Object.keys(clients).length);
-      console.log('chatSessions.length', chatSessions.length);
+// Cors
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8000', 'https://saf-web-client.vercel.app']; // add any other origins that you want to allow
 
-      socket.emit('receive message', chatSessions);
-    })
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin
+    if (!origin) return callback(null, true);
 
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
 
-      // Remove them from the clients object
-      for (const userId in clients) {
-        if (clients[userId] === socket) {
-          delete clients[userId];
-          break;
-        }
-      }
-    })
+    return callback(null, true);
+  },
+  credentials: true // enable passing cookies from the client to the server
+}));
 
-    socket.on('send message', async ({ userId, message, sessionId }) => {
-      // Fetch the ChatSession for this user id and return it through emitting 'receive message' event
-      const chatSession = await ChatSession.findOne({
-        _id: sessionId,
-      }).populate('participants').populate('messages').exec();
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-      // Save the message to the database
-      const chatMessage = await ChatMessage.create({
-        sender: userId,
-        recipient: chatSession.participants.find((participant) => participant._id.toString() !== userId),
-        text: message,
-      });
+// Middleware
+app.use(errorHandler);
 
-      // Send the message to the other participant
-      const otherParticipant = chatSession.participants.find((participant) => participant._id.toString() !== userId);
-      // console.log('otherParticipant', otherParticipant._id);
-      console.log('otherParticipant.name', otherParticipant.name);
-      // clients[otherParticipant].emit('receive message', chatMessage);
+// Common Routes
+app.use('/api/users', require('./routes/user.routes'));
+app.use('/api/auth', require('./routes/auth.routes'));
 
-      // Add the message to the chat session
-      chatSession.messages.push(chatMessage);
-      await chatSession.save();
+// Social Routes
+app.use('/api/shares', require('./routes/social/share.routes'));
+app.use('/api/likes', require('./routes/social/like.routes'));
+app.use('/api/comments', require('./routes/social/comment.routes'));
+app.use('/api/posts', require('./routes/social/post.routes'));
 
-      // Emit an event to the other participant to update their list of chat sessions, return all chat sessions
-      const updatedChatSessions = await ChatSession.find({
-        participants: otherParticipant,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
+// Ecommerce Routes
+app.use('/api/add-to-cart', require('./routes/ecom/addToCart.routes'));
+app.use('/api/orders', require('./routes/ecom/order.routes'));
+app.use('/api/products', require('./routes/ecom/product.routes'));
+app.use('/api/product-variants', require('./routes/ecom/productVariant.routes'));
+app.use('/api/product-categories', require('./routes/ecom/productCategory.routes'));
+app.use('/api/promo-codes', require('./routes/ecom/promoCode.routes'));
+app.use('/api/shop-location', require('./routes/ecom/shopLocation.routes'));
+app.use('/api/wishlist', require('./routes/ecom/wishlist.routes'));
 
-      // Check if the other participant is still connected
-      if (!clients[otherParticipant._id]) {
-        // console.log('clients[otherParticipant]', clients[otherParticipant._id])
-        // console.log('clients.length', Object.keys(clients).length);
-        console.log('Other participant is not connected');
-      } else {
-        // console.log('clients[otherParticipant]', clients[otherParticipant._id])
-        // console.log('clients[otherParticipant].name', otherParticipant.name);
-        clients[otherParticipant._id].emit('receive message', updatedChatSessions);
-      }
+// Statistics Routes
+app.use('/api/statistics', require('./routes/statistics/statistics.routes'));
 
-      // Emit an event to the sender to update their list of chat sessions, return all chat sessions
-      const updatedChatSessionsUser = await ChatSession.find({
-        participants: userId,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
-      clients[userId].emit('receive message', updatedChatSessionsUser);      
-    })
+// Fetch Image
+app.use('/uploads', express.static('uploads'));
 
-    socket.on('customer:contactSupport', async (userId) => {
-      console.log('Customer contacting support:', userId);
+// Temp
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-      // Check which support agent has the lowest number of active chat sessions
-      const supportAgents = await User.find({
-        role: 'Support',
-      });
-      const supportAgentChatSessions = await Promise.all(supportAgents.map(async (supportAgent) => {
-        const chatSessions = await ChatSession.find({
-          participants: supportAgent._id,
-          status: 'active',
-        }).populate('participants').populate('messages').exec();
-        return {
-          supportAgent,
-          chatSessions,
-        };
-      }));
+// Start the server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
-      const supportAgentWithLowestChatSessions = supportAgentChatSessions.reduce((prev, curr) => {
-        if (prev.chatSessions.length < curr.chatSessions.length) {
-          return prev;
-        }
-        return curr;
-      });
+// Start the Socket.io server
+socketServer(server);
 
-      // Create a new ChatSession with the customer and the support agent
-      const user = await User.findById(userId);
-      const chatSession = await ChatSession.create({
-        name: `${user.name}`,
-        participants: [userId, supportAgentWithLowestChatSessions.supportAgent._id],
-        status: 'active',
-      });
-
-      // Add the chat session to the clients object, return all chat sessions
-      const chatSessions = await ChatSession.find({
-        participants: userId,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
-      clients[userId].emit('receive message', chatSessions);
-
-      // Emit an event to the support agent to update their list of chat sessions
-      const updatedChatSessions = await ChatSession.find({
-        participants: supportAgentWithLowestChatSessions.supportAgent._id,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
-      clients[supportAgentWithLowestChatSessions.supportAgent._id].emit('receive message', updatedChatSessions);
-    });
-
-    socket.on('message read', async (userId, chatSessionId) => {
-      console.log('Message read:', chatSessionId);
-
-      // Update the ChatSession in the database
-      const chatSession = await ChatSession.findById(chatSessionId).populate('participants').populate('messages').exec();
-      // Loop through the messages and update the read status of the ones that are unread
-      chatSession.messages.forEach((message) => {
-        if (message.read === false && message.sender.toString() !== userId) {
-          message.isRead = true;
-        }
-      });
-      await chatSession.save();
-
-      // Emit an event to the other participant to update their list of chat sessions
-      const otherParticipant = chatSession.participants.find((participant) => participant !== userId);
-      const updatedChatSessions = await ChatSession.find({
-        participants: otherParticipant,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
-      clients[otherParticipant].emit('message seen', updatedChatSessions);
-    });
-
-    socket.on('support:markAsDone', async ({userId, sessionId}) => {
-      console.log('Support marking chat session as done:', sessionId);
-
-      // console.log('sessionId', sessionId);
-
-      // Update the ChatSession in the database
-      const chatSession = await ChatSession.findById(sessionId).populate('participants').populate('messages').exec();
-      chatSession.status = 'closed';
-      await chatSession.save();
-
-      // Emit an event to the other participant to update their list of chat sessions
-      const otherParticipant = chatSession.participants.find((participant) => participant._id !== userId);
-      console.log('otherParticipant', otherParticipant._id);
-      clients[otherParticipant._id].emit('ticket closed', chatSession);
-
-      // Emit an event to the support agent to update their list of chat sessions
-      const updatedChatSessions = await ChatSession.find({
-        participants: userId,
-        status: 'active',
-      }).populate('participants').populate('messages').exec();
-
-      clients[userId].emit('receive message', updatedChatSessions);
-
-    });
-  });
-};
+module.exports = app;
